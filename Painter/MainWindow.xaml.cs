074e9546180f67843ca21376a673490ff90b6f3d
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-06-22 21:56:30 +0900

    feat : Add Canvas resize

--- Painter/MainWindow.xaml.cs
+++ Painter/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace Painter
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<object> paintedShape;
+        
+
         public MainWindow()
         {
             InitializeComponent();
+            paintedShape = new List<object>();
         }
+
+        public void DrawLine(object sender, RoutedEventArgs e)
+        {
+            Line line = new Line();
+        }
+
+        // Drag정도에 따라 캔버스 크기 조정
+        private void Canvas_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            //Move the Thumb to the mouse position during the drag operation
+            double yadjust = Canvas.Height + e.VerticalChange / 10;
+            double xadjust = Canvas.Width + e.HorizontalChange / 10;
+
+            if ((xadjust >= 100) && (yadjust >= 100))
+            {
+                Canvas.Width = xadjust;
+                Canvas.Height = yadjust;
+                Canvas.SetLeft(CanvasThumb, Canvas.GetLeft(CanvasThumb) +
+                                        e.HorizontalChange);
+                Canvas.SetTop(CanvasThumb, Canvas.GetTop(CanvasThumb) +
+                                        e.VerticalChange);
+            }
+        }
+
+        private void Canvas_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            CanvasThumb.Background = Brushes.Orange;
+        }
+
+        private void Canvas_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            CanvasThumb.Background = Brushes.LightGray;
+        }
+
     }
 }
